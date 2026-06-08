1783eeb74a4887576d7714deb4d112ae6837c2c6
Jamie Wong <49280271+zhexingtianxia@users.noreply.github.com>
2026-06-08 11:31:19 +0800

    Update CaseDetail.tsx

--- src/pages/CaseDetail.tsx
+++ src/pages/CaseDetail.tsx
@@ -14,7 +14,7 @@ const casesData = {
       "/case1.jpg", "/case2.jpg", "/case3.jpg", "/case4.jpg", "/case5.jpg", 
       "/case6.jpg", "/case7.jpg", "/case8.jpg", "/case9.jpg", "/case10.jpg", 
       "/case11.jpg", "/case12.jpg", "/case13.jpg", "/case14.jpg", "/case15.jpg",
-      "/case16.jpg", "/case17.jpg"
+      "/case16.jpg", "/case17.jpg", "/case18.jpg"
     ] 
   },
   "2": { 
