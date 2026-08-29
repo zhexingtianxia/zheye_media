65aa9c09dff341da015a862f0811e30117a8a924
Jamie Wong <49280271+zhexingtianxia@users.noreply.github.com>
2026-08-29 13:57:59 +0800

    new cases and baokuan

--- src/pages/CaseDetail.tsx
+++ src/pages/CaseDetail.tsx
@@ -14,7 +14,8 @@ const casesData = {
       "/case1.jpg", "/case2.jpg", "/case3.jpg", "/case4.jpg", "/case5.jpg", 
       "/case6.jpg", "/case7.jpg", "/case8.jpg", "/case9.jpg", "/case10.jpg", 
       "/case11.jpg", "/case12.jpg", "/case13.jpg", "/case14.jpg", "/case15.jpg",
-      "/case16.jpg", "/case17.jpg", "/case18.jpg", "/case19.jpg"
+      "/case16.jpg", "/case17.jpg", "/case18.jpg", "/case19.jpg", "/case20.jpg",
+      "/case21.jpg","/case22.jpg","/case23.jpg"
     ] 
   },
   "2": { 
@@ -34,7 +35,7 @@ const casesData = {
       "/kezi6.jpg", "/kezi7.png","/kezi8.jpg", "/kezi9.jpg", "/kezi10.jpg",
       "/kezi11.jpg", "/kezi12.jpg", "/kezi13.jpg", "/kezi14.jpg", "/kezi15.jpg", 
       "/kezi16.jpg", "/kezi17.jpg", "/kezi18.jpg", "/kezi19.jpg", "/kezi20.jpg",
-      "/kezi21.jpg", "/kezi22.jpg"
+      "/kezi21.jpg", "/kezi22.jpg", "/kezi23.jpg"
     ] 
   },
   "4": { 
@@ -57,7 +58,8 @@ const casesData = {
     images: [
       "/baokuan1.jpg", "/baokuan2.jpg", "/baokuan3.jpg", "/baokuan4.jpg", 
       "/baokuan5.jpg", "/baokuan6.jpg", "/baokuan7.jpg", "/baokuan8.jpg", 
-      "/baokuan9.jpg"
+      "/baokuan9.jpg", "/baokuan10.jpg", "/baokuan11.jpg", "/baokuan12.jpg",
+      "/baokuan13.jpg"
     ] 
   }
 };
